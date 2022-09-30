161712c5ddb713bcf496e95a80115e492c05237f
Aki <arishok.kun1009@gmail.com>
2022-09-30 21:16:10 +1000

    tusks 2 and 4 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+/*
+Console.Write("input number one: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input number two: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+int min;
+
+if(number1 > number2)
+{
+    max = number1;
+    min = number2;
+}
+else
+{
+    max = number2;
+    min = number1;
+}
+Console.WriteLine($"Max digit is {max} and min digit is {min} ");
+*/
+
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Input first digit: ");
+int dig1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second digit: ");
+int dig2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third digit: ");
+int dig3 = Convert.ToInt32(Console.ReadLine());
+
+int max = dig1;
+
+if(max < dig2){
+     max = dig2;
+}
+if(max < dig3){
+    max = dig3;
+}
+Console.WriteLine("Max digit is " + max);
+
+
