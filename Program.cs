262d449f11fe221f2665055aa68e20fe3b7c5de6
Aki <arishok.kun1009@gmail.com>
2022-10-17 17:35:57 +1000

    Dz5

--- Program.cs
+++ Program.cs
@@ -1,85 +1,9 @@
-﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-3, 5 -> 243 (3⁵)
-2, 4 -> 16
-*/
-int InputNum(string n)
+﻿int InputNum(string n)
 {
     Console.Write($"Input {n} number: ");
     int num = Convert.ToInt32(Console.ReadLine());
     return num;
 }
-/*
-int Stepen(int a, int b)
-{
-    int comp = 1;
-
-    for(int cur = 1; cur <= b; cur++)
-    
-        comp *= a;
-
-    return comp;
-}
-
-int a = InputNum("A");
-int b = InputNum("B");
-
-int num1 = Stepen(a, b);
-
-Console.WriteLine(num1);
-*/
-
-/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-452 -> 11
-82 -> 10
-9012 -> 12
-*/
-/*
-//variant 1
-
-int SumDigits(int num)
-{
-    int sum = 0;
-
-    while(num != 0){
-        if(num < 0) num *= -1;
-        int num1 = num % 10;
-        num = num / 10;
-        sum = sum + num1;
-    }
-    return sum;
-}
-*/
-/*
-//variant 2
-
-int SumDigits(int num)
-{
-    string str = num.ToString();
-    int len = str.Length;
-    int[] nums = new int[len];
-    int sum = 0;
-    int count = 0;
-    while(count < len){
-        nums[count] = Convert.ToInt32(str[count].ToString());
-        count++;
-    }
-
-    for(int i = 0; i < len; i++) 
-        sum += nums[i];
-    return sum;
-}
-
-int number = InputNum(string.Empty);
-
-int summ = SumDigits(number);
-
-Console.WriteLine(summ);
-*/
-
-/*Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-m = 5 -> [1, 2, 5, 7, 19]
-m = 3 -> [6, 1, 33]
-*/
 
 void ShowArray(int[] array)
 {
@@ -87,47 +11,143 @@ void ShowArray(int[] array)
         Console.Write(array[i] + " ");
     Console.WriteLine();
 }
-/*
-int[] RandomArray(int size, int minValue, int maxValue)
+
+int[] RandomArray(int size, int min, int max)
 {
     int[] array = new int[size];
+
     for(int i = 0; i < size; i++)
-        array[i] = new Random().Next(minValue, maxValue);
+        array[i] = new Random().Next(min, max);
+
     return array;
 }
 
+/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
 
-int m = InputNum("m");
+/*int[] Random3DigitArray(int size)
+{
+    int[] array = new int[size];
 
-int[] myArray = RandomArray(m, 0, 100);
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(100, 1000);
+
+    return array;
+}
+
+int EvenNumber(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] % 2 == 0) count++;
+
+    return count;
+}
+
+int size = InputNum("size");
+int[] myArray = Random3DigitArray(size);
 
 ShowArray(myArray);
+int evenNumber = EvenNumber(myArray);
+
+Console.WriteLine(evenNumber);
 */
 
-//доп. задание: создание массива с элементами от пользователя
-//если инкорректный ввод сайза
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
 
-int[] CreateArray(int size)
+/*int SumUneven(int[] array)
 {
-int[] newArray = new int[size];
+    int sum = 0;
 
-for(int i = 0; i < size; i++){
-    Console.Write("input number: ");
-    newArray[i] = Convert.ToInt32(Console.ReadLine());
+    for(int i = 0; i < array.Length; i++)
+        if(i % 2 != 0) sum += array[i];
+
+    return sum;
 }
-return newArray;
-}
 
-int size1 = InputNum("size");
+int size = InputNum("size");
+int min = InputNum("minimum");
+int max = InputNum("maximum");
+int[] notMyArray = RandomArray(size, min, max);
 
-if(size1 < 0) size1 *= -1;
+ShowArray(notMyArray);
+int sum = SumUneven(notMyArray);
 
-int[] massiv = CreateArray(size1);
-
-/*while(size1 < 0){
-    Console.WriteLine("Incorrect input");
-    Console.Write("input size: ");
-    size1 = Convert.ToInt32(Console.ReadLine());
-}
+Console.WriteLine(sum);
 */
-ShowArray(massiv);
+
+/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+/*
+double[] RandomDoubleArray(int size)
+{
+    double[] array = new double[size];
+
+    for(int i = 0; i < size; i++){
+        double rnd = new Random().NextDouble();
+        array[i] = rnd * new Random().Next(-100, 100);                  //если только для положительных, соответственно никаких -100 здесь не нужно.
+        array[i] = Math.Round(array[i], new Random().Next(1, 5));
+    }
+    return array;
+}
+
+void ShowDArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Double Difference(double[] array)
+{
+    double diff = 0;
+    double minI = array[0];
+    double maxI = array[0];
+    for(int i = 1; i < array.Length; i++){
+        if(array[i] > maxI) maxI = array[i];
+        else if(array[i] < minI) minI = array[i];
+    }
+    
+    diff = maxI - minI;
+    Console.WriteLine($"{maxI} - {minI} = {diff}");
+    return diff;
+}
+
+int size = InputNum("size");
+double[] myArray = RandomDoubleArray(size);
+ShowDArray(myArray);
+double num = Difference(myArray);
+*/
+
+/*Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и 
+предпоследний и т.д. Результат запишите в новом массиве.
+Ну и примеры:
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21*/
+
+int[] CompArray(int[] array)
+{
+    int size = array.Length / 2 + array.Length % 2;
+    int size1 = array.Length;
+    int[] newAraray = new int[size];
+
+    for(int i = 0; i < size; i++){
+        if(i != size1 - 1){
+            newAraray[i] = array[i] * array[size1 - 1];
+            size1 = size1 - 1;
+        }
+        else newAraray[i] = array[i];
+    }
+    return newAraray;
+}
+
+int size = InputNum("size");
+int min = InputNum("minimum");
+int max = InputNum("maximum");
+int[] array32 = RandomArray(size, min, max);
+
+ShowArray(array32);
+int[] compAR = CompArray(array32);
+ShowArray(compAR);
