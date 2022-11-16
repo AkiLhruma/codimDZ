aa228ed55b0ab86d72709258c4e847bf592c7962
Aki <arishok.kun1009@gmail.com>
2022-11-16 16:49:02 +1000

    pravki

--- Program.cs
+++ Program.cs
@@ -4,23 +4,19 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
 
 void Row(int n)
 {
-    Console.Write(n + ", ");
-    if(n > 1) Row(n-1);
+    if(n <=1) Console.WriteLine(1);
+    else
+    {
+        Console.Write(n + ", ");
+        if(n > 1) Row(n-1);
+    }
 }
-/*
-int num;
-do
-{
-    Console.Write("Input N: ");
-    num = Convert.ToInt32(Console.ReadLine());
-    if(num > 0) break;
-    Console.WriteLine("Incorrect input. Try again");
-    Console.WriteLine();
-}
-while(num <= 1);
+
+Console.Write("Input N: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
 Row(num);
-*/
+
 
 
 /*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
@@ -29,27 +25,26 @@ M = 4; N = 8. -> 30*/
 
 int Summ(int m, int n)
 {
-    int sum = 0;
+    if(m < 0) return Summ(m+1,n);
+    if (n < 0) return Summ(m, n+1);   //изначально всё это было так просто, но пока делал что бы это работало с любыми вводными я чуть не сломал себе мозг
 
-    for(int count = m; count <= n; count++) 
-        if(count > 0) sum += count;
-
-    return sum;
+    if(m < n)  
+    {
+        m += Summ(m+1,n);
+        return m;
+    }
+    if(n < m) 
+    {
+        m += Summ(m-1, n);
+        return m;
+    }
+    return m;
 }
 /*
-int num1;
-int num2;
-do
-{
-    Console.Write("Input M: ");
-    num1 = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input N: ");
-    num2 = Convert.ToInt32(Console.ReadLine());
-    if(num1 < num2) break;
-    Console.WriteLine("Incorrect input. Try again");
-    Console.WriteLine();
-}
-while(num1 >= num2);
+Console.Write("Input M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
 int res = Summ(num1, num2);
 Console.WriteLine("Summ of elements from M to N is " + res);
@@ -66,7 +61,7 @@ long Akerman(long m, long n)
     else if(m > 0 && n == 0) return Akerman(m-1, 1);
     else return Akerman(m-1, Akerman(m, n-1));
 }
-
+/*
 int num1;
 int num2;
 do
@@ -83,3 +78,4 @@ while(num1 < 0 | num2 < 0);
 
 long a = Akerman(num1, num2);
 Console.WriteLine(a);
+*/
